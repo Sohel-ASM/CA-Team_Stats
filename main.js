b8bd913927221eb8772a9f3e1a88b66dfcff6b49
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-10 10:02:31 -0400

    team object, its properties, and getter methods

---
+++ main.js
@@ -0,0 +1,45 @@
+// creating a team object with _players and -games properties in it. The properties' values are arrays.
+const teamBarca = {
+    _players: [
+        {
+            firstName: 'Lionel',
+            lastName: 'Messi',
+            age: 32
+    },
+        {
+            firstName: 'Neymar',
+            lastName: 'Junior',
+            age: 27
+    },
+        {
+            firstName: 'Andres',
+            lastName: 'Iniesta',
+            age: 28
+    }
+
+    ],
+    _games: [
+        {
+            opponent: 'Bayern Munich',
+            teamGoal: 1,
+            opponentGoal: 1
+    },
+        {
+            opponent: 'Real Madrid',
+            teamGoal: 2,
+            opponentGoal: 1
+    },
+        {
+            opponent: 'Man Utd.',
+            teamGoal: 3,
+            opponentGoal: 2
+    }
+    ],
+    //getter methods for _players and _games. Program users are not allowed to change these properties' values, so no setter methods. 
+    get players() {
+        return this._players;
+    },
+    get games() {
+        return this._games;
+    },
+}; // this is const's closing closing brace
